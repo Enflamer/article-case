cd7b3e63bb86f310af7bff4622753c05ad6eae5f
withe <enflaimer@gmail.com>
2020-09-19 23:47:20 +0300

    article components and main logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import React from "react";
-import Art from "./components/Article";
+import React, { useState, useEffect } from "react";
+import Article from "./components/Article";
 import "./App.css";
 import "./style/logbook.scss";
 import "./style/navbar.scss";
+import axios from "axios";
 
-function App() {
+export default function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://sweb.ru/export/turbojournal/", {})
+      .then((response) => {
+        var convert = require("xml-js");
+        const result = convert.xml2json(response.data, {
+          compact: true,
+          spaces: 4,
+        });
+        let json = JSON.parse(result);
+        console.log(json.rss.channel.item['title']);
+        setArticles(json.rss.channel.item);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+
+
   return (
     <div className="app">
       <div className="navbar"></div>
@@ -12,11 +35,14 @@ function App() {
         <div className="logbook-page">
           <div className="logbook-page__title">Бортовой журнал</div>
           <div className="logbook-page__title">Бортовой журнал</div>
-          <Art></Art>
+          {articles.map((item, index) => (
+            <Article 
+              key={index} 
+              title={item.title}
+            />
+          ))}
         </div>
       </div>
     </div>
   );
 }
-
-export default App;
--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,14 @@
-import React from 'react'
-import '../style/article.scss'
+import React from "react";
+import "../style/article.scss";
+// import parser from "xml2json"
 
-export default function article() {
-    return (
-        <div className="article">
-            <hr className='article__line'></hr>
-            Топ-6 сервисов для проверки кроссбраузерности сайта
-            <hr className='article__line'></hr>
-        </div>
-    )
+export default function Article() {
+  return (
+    <div className="article">
+      <hr className="article__line"></hr>
+      {/* Топ-6 сервисов для проверки кроссбраузерности сайта */}
+      {}
+      <hr className="article__line"></hr>
+    </div>
+  );
 }
